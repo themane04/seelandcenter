80d23915fd353aa2123bd9c3de7a00cdec2cb3bf
mane <tomevmarjan33@gmail.com>
2024-10-27 16:34:57 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Text} from "@chakra-ui/react";
+
+function App() {
+    return (
+        <>
+            <Text>
+                Hello, World!
+            </Text>
+        </>
+    )
+}
+
+export default App
